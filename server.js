9ef4fec39240a8183656fb7f71054bd03eddd33f
gabrilr <gabrilreyes30@gmail.com>
2023-07-24 21:44:41 -0600

    Afinando detalles

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mysql = require('mysql2');
 const myconn = require('express-myconnection');
 const cors = require('cors');
 const dotenv = require('dotenv');
-
 const routes = require('./routes');
 
 dotenv.config();
@@ -11,18 +10,17 @@ const app = express();
 
 app.set('port', process.env.PORT || 3001);
 
-
 // middlewares
-
 const dbOptions = {
 
     host: process.env.HOST,
     port: process.env.PORT_DB,
-    user: process.env.USER,   
+    user: process.env.USER_DB,   
     password: process.env.PASSWORD,
     database: process.env.DB
 
 }
+//console.log(process.env.HOST +' : '+ process.env.USER +' : '+ process.env.PASSWORD);
 
 app.use(myconn(mysql, dbOptions, 'single'))
 app.use(express.json())
@@ -31,16 +29,13 @@ app.use(cors())
 //routes-------------------------
 app.get('/', (req, res) => {
 
-    res.send('Pagina principal');
-
+    res.send('<center><h1>Bienvenido a Cadofi</h1></center>');
 })
 
-
-app.use('/api', routes)
+app.use('/', routes)
 
 // server running----------------------------
 app.listen(app.get('port'), () => {
 
-    console.log('Server running on port', app.get('port'))
-
+    console.log('Servidor a la escucha en el puerto:', app.get('port'))
 })
